89a578dd2c6acd2b08aa074f96dc54483a1c95b4
Anand Manissery <anand.manissery@gmail.com>
2019-01-25 18:57:41 +0530

    fix(test): Fixed Nav component test cases by adding routing test module and removed useless code from app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {HomeModule} from './routes/home/home.module';
+import {NavModule} from './shared/components/nav/nav.module';
+import {DashboardModule} from './routes/dashboard/dashboard.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        NavModule,
+        HomeModule,
+        DashboardModule
       ],
       declarations: [
         AppComponent
@@ -18,18 +24,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'angular-whiteapp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-whiteapp');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-whiteapp!');
   });
 });
